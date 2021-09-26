429ba8fac681eafac54be18098b61e21bf8ae1e4
User <User@Flammrock>
2021-09-27 01:18:33 +0200

    update

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -36,18 +36,30 @@ public class TP1 {
         ArrayList<Talisman> t = new ArrayList();
         t.add(medaillon1);
         t.add(medaillon2);
-        Room salle = new Room(0,0,"salle de coffre","NSE",t);
-        Character Hero = new Character(salle,"Hero",null);
-        ArrayList<Room> r = new ArrayList();
-        r.add(salle);
-        Map m =  new Map(r,Hero,1,1);
+        //Room salle = new Room(0,0,"salle de coffre","NSE",t);
         
+        //ArrayList<Room> r = new ArrayList();
+        //r.add(salle);
+        
+        // on créé une map vide
+        Map m =  new Map();
+        
+        // on construit la map à partir du fichier
         try {
             m.buildFromInputStream(TP1.class.getResourceAsStream("Rooms.dat"));
-            m.enterNewRoom(salle);
         } catch (Exception e) {
             System.out.println("fail build rooms");
         }
+        
+        // on créé le héro et on le met dans la 1ère salle de la liste
+        Character Hero = new Character(m.getRooms().get(0),"Hero",null);
+        
+        // on ajoute le héro dans la map
+        m.setHero(Hero);
+        
+        // on fait entrer le héro dans la salle
+        m.enterNewRoom(m.getRooms().get(0));
+        
         
         
         
