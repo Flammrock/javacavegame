bed1ab304bf5b8e68c0c4dad2ebaf2828a6cef9e
User <User@Flammrock>
2021-09-27 02:04:04 +0200

    update

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -42,27 +42,24 @@ public class TP1 {
         //r.add(salle);
         
         // on créé une map vide
-        Map m =  new Map();
-        
-        // on construit la map à partir du fichier
         try {
-            m.buildFromInputStream(TP1.class.getResourceAsStream("Rooms.dat"));
+            Map m =  new Map.Builder()
+                    .roomsFromStream(new Stream(TP1.class.getResourceAsStream("Rooms.dat")))
+                    .build();
+
+            // on créé le héro et on le met dans la 1ère salle de la liste
+            Character Hero = new Character(m.getRooms().get(0),"Hero",null);
+
+            // on ajoute le héro dans la map
+            m.setHero(Hero);
+
+            // on fait entrer le héro dans la salle
+            m.enterNewRoom(m.getRooms().get(0));
+            m.quefaire();
+            
         } catch (Exception e) {
             System.out.println("fail build rooms");
         }
-        
-        // on ajoute les talismans
-        
-        // on créé le héro et on le met dans la 1ère salle de la liste
-        Character Hero = new Character(m.getRooms().get(0),"Hero",null);
-        
-        // on ajoute le héro dans la map
-        m.setHero(Hero);
-        
-        // on fait entrer le héro dans la salle
-        m.enterNewRoom(m.getRooms().get(0));
-        m.quefaire();
-        
         
         
     }
